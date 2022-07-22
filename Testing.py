405c8bc4324ae682ed55f5a837d5a38005719734
Guled Ali <99419181+GuYOAli@users.noreply.github.com>
2022-07-22 04:58:06 -0700

    Update Selenium module to last version.
    I was using auto-dated Selenium module and I upgraded it to the last version.

--- Testing.py
+++ Testing.py
@@ -1,58 +1,56 @@
-from tkinter import Select
 from selenium import webdriver
+from selenium.webdriver.support.select import Select
 
 browser = webdriver.Chrome()
 
 # heading to the site
 browser.get("https://jupiter.cloud.planittesting.com/#/")
 
-# 1. Clicking the Login botton
-login = browser.find_element_by_link_text("Login")
+# 1. Click the log in Button
+login = browser.find_element("id", "nav-login")
 login.click()
 
-# 2. Testing the user name filed
-username = browser.find_element_by_id("loginUserName")
-username.send_keys("Guled Ali")
+# Testing the Login form
+# 2. User name "Guled Ali"
+user_name = browser.find_element("id", "loginUserName")
+user_name.send_keys("Guled Ali")
 
-# 3. Testing the Password filed
-password = browser.find_element_by_id("loginPassword")
-password.send_keys("I don't know")
+# 3. Password
+password = browser.find_element("id", "loginPassword")
+password.send_keys("Hello123")
 
+# Submit the form
 password.submit()
 
 # 4. Testing if the user name is displayed on the page
 assert "Guled Ali" in browser.page_source
 
+# 5. Testing the cart button
 
-# 7. Testing the cart button
-# the reason I'm using find_element_by_link_text is there is no specific class name or ID
-
-cart = browser.find_element_by_link_text("Cart")
+cart = browser.find_element("id", "nav-cart")
 cart.click()
 
-
 # 6. Testing the Forename filed
-first_name = browser.find_element_by_id("forename")
-first_name.send_keys("Guled")
-
+forename = browser.find_element("id", "forename")
+forename.send_keys("Guled")
 
 # 7. Testing the Email filed
-buy = browser.find_element_by_id("email")
-buy.send_keys("hello@gamil.com")
 
+email = browser.find_element("id", "email")
+email.send_keys("hello@gmail.com")
 
 # 8. Testing the address filed
 
-address = browser.find_element_by_id("address")
-address.send_keys("123 Brisbane QLD 114 Australia")
+address = browser.find_element("id", "address")
+address.send_keys("123 Brisbane 419 QLD Australia")
 
-# 9. Testing the card Type dropdwon list and selecting by index
+# 9. Testing the card Type and selecting by index
 
-card_type = browser.find_element_by_id("cardType")
-cardType = Select(card_type)
-cardType.select_by_index(1)
+card_type = browser.find_element("id", "cardType")
+select = Select(card_type)
+select.select_by_index(1)
 
-# 10. Testing the card number filed
+# 10. Testing card number filed
 
-card = browser.find_element_by_id("card")
-card.send_keys("1234567890")
+card_number = browser.find_element("id", "card")
+card_number.send_keys("0123456789")
